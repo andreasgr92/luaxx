c26669f10e08893918caff526c92c4fb187d4b69
Leon Kasovan <leonkasovan@gmail.com>
2023-07-12 05:15:31 +0700

    add Makefile for msys fix build_for_msys2.sh

--- build_for_msys2.sh
+++ build_for_msys2.sh
@@ -7,16 +7,22 @@
 
 cd external
 
-# git clone https://github.com/leonkasovan/zlib.git
-# cd zlib
-# cmake .
-# ninja
-# cd ..
+if [[ ! -d "zlib" ]]; then
+    git clone https://github.com/leonkasovan/zlib.git
+fi
+cd zlib
+cmake .
+ninja
+pwd
+cd ..
 
-git clone https://github.com/leonkasovan/curl.git
+if [[ ! -d "curl" ]]; then
+    git clone https://github.com/leonkasovan/curl.git
+fi
 cd curl
 ./buildconf 
 ./configure --with-schannel --without-brotli --without-nghttp2 --without-libidn2 --without-libpsl --without-zstd --disable-alt-svc --disable-hsts --disable-tls-srp --disable-proxy --disable-ipv6 --disable-ntlm-wb --disable-ntlm --disable-dict --disable-gopher --disable-gophers --disable-imap --disable-mqtt --disable-pop3 --disable-pop3s --disable-smtp --disable-smtps --disable-telnet --disable-tftp --disable-rtsp --disable-file --disable-ldap --disable-ldaps --disable-pthreads
 make
 
-
+cd ../..    # back to /f/Projects/luaxx/
+make
